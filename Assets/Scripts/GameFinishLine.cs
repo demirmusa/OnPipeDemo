8fa23ec9746dc159d8083d9af71443266e4d9827
Musa Demir <musa.demir@volosoft.com>
2020-10-23 23:07:36 +0300

    bitiş eklendi

---
+++ Assets/Scripts/GameFinishLine.cs
@@ -0,0 +1,19 @@
+﻿using Managers;
+using UnityEngine;
+
+/// <summary>
+/// GameFinishLine objeler GameFinishLine layerında ve bu layer da yalnızca player ile ile etkileşime geçebilir. Layer Collusion Matrixinden bu şekilde ayarlandı.
+/// </summary>
+public class GameFinishLine : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameManager.Instance.SetLevelComplete();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
